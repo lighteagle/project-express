592ab8fbaf238b85522bc3c1f23625adaa6b914d
lighteagle <sofyan.agus18@gmail.com>
2019-11-27 13:40:41 +0700

    add env

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
 const bodyParser = require("body-parser");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const todoList = [
   {
